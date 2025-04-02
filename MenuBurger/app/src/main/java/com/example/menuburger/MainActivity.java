d2a8ad759be3b96bc5384f97676f8fe0c879fa4d
favale.cloe <08.dwam2024@gmail.com>
2025-04-02 16:12:29 +0200

    02/04/2025

--- MenuBurger/app/src/main/java/com/example/menuburger/MainActivity.java
+++ MenuBurger/app/src/main/java/com/example/menuburger/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
@@ -28,10 +30,50 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new HomeFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        // return false;
+
+        if(item.getItemId()==R.id.nav_home) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new HomeFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+        if(item.getItemId()==R.id.nav_profile) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new ProfileFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_profile);
+        }
+
+        if(item.getItemId()==R.id.nav_messages) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new MessageFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_messages);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
